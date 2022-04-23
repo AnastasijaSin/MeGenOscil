db6a34ce7776f6736bf172caa01e9993edfc1306
AnastasijaSin <103102745+AnastasijaSin@users.noreply.github.com>
2022-04-23 17:07:42 +0300

    MeGenOscil
    Oscilografo ir signalo generatoriaus programa

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MyGenOscill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSin = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nudAmplitude = new System.Windows.Forms.NumericUpDown();
+            this.nudFrequency = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClearConsole = new System.Windows.Forms.Button();
+            this.btnSawtooth = new System.Windows.Forms.Button();
+            this.btnClearPanel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplitude)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrequency)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSin
+            // 
+            this.btnSin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSin.Location = new System.Drawing.Point(25, 36);
+            this.btnSin.Name = "btnSin";
+            this.btnSin.Size = new System.Drawing.Size(75, 39);
+            this.btnSin.TabIndex = 0;
+            this.btnSin.Text = "Sine";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSquare.Location = new System.Drawing.Point(25, 126);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(75, 39);
+            this.btnSquare.TabIndex = 1;
+            this.btnSquare.Text = "Square";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTriangle.Location = new System.Drawing.Point(25, 81);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(75, 39);
+            this.btnTriangle.TabIndex = 2;
+            this.btnTriangle.Text = "Triangle";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(123, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 274);
+            this.panel1.TabIndex = 3;
+            // 
+            // nudAmplitude
+            // 
+            this.nudAmplitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudAmplitude.Location = new System.Drawing.Point(507, 136);
+            this.nudAmplitude.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAmplitude.Name = "nudAmplitude";
+            this.nudAmplitude.Size = new System.Drawing.Size(60, 26);
+            this.nudAmplitude.TabIndex = 4;
+            this.nudAmplitude.Value = new decimal(new int[] {
+            75,
+            0,
+            0,
+            0});
+            this.nudAmplitude.ValueChanged += new System.EventHandler(this.nudAmplitude_ValueChanged);
+            // 
+            // nudFrequency
+            // 
+            this.nudFrequency.DecimalPlaces = 1;
+            this.nudFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudFrequency.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudFrequency.Location = new System.Drawing.Point(308, 303);
+            this.nudFrequency.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudFrequency.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudFrequency.Name = "nudFrequency";
+            this.nudFrequency.Size = new System.Drawing.Size(60, 26);
+            this.nudFrequency.TabIndex = 5;
+            this.nudFrequency.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudFrequency.ValueChanged += new System.EventHandler(this.nudFrequency_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(504, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Amplitude, V";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(202, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Frequency, Hz";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnClearConsole
+            // 
+            this.btnClearConsole.Location = new System.Drawing.Point(25, 292);
+            this.btnClearConsole.Name = "btnClearConsole";
+            this.btnClearConsole.Size = new System.Drawing.Size(75, 46);
+            this.btnClearConsole.TabIndex = 8;
+            this.btnClearConsole.Text = "Clear Console";
+            this.btnClearConsole.UseVisualStyleBackColor = true;
+            this.btnClearConsole.Click += new System.EventHandler(this.btnClearConsole_Click);
+            // 
+            // btnSawtooth
+            // 
+            this.btnSawtooth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSawtooth.Location = new System.Drawing.Point(25, 171);
+            this.btnSawtooth.Name = "btnSawtooth";
+            this.btnSawtooth.Size = new System.Drawing.Size(75, 39);
+            this.btnSawtooth.TabIndex = 9;
+            this.btnSawtooth.Text = "Sawtooth";
+            this.btnSawtooth.UseVisualStyleBackColor = true;
+            this.btnSawtooth.Click += new System.EventHandler(this.btnSawtooth_Click);
+            // 
+            // btnClearPanel
+            // 
+            this.btnClearPanel.Location = new System.Drawing.Point(25, 234);
+            this.btnClearPanel.Name = "btnClearPanel";
+            this.btnClearPanel.Size = new System.Drawing.Size(75, 52);
+            this.btnClearPanel.TabIndex = 10;
+            this.btnClearPanel.Text = "Clear Panel";
+            this.btnClearPanel.UseVisualStyleBackColor = true;
+            this.btnClearPanel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 341);
+            this.Controls.Add(this.btnClearPanel);
+            this.Controls.Add(this.btnSawtooth);
+            this.Controls.Add(this.btnClearConsole);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudFrequency);
+            this.Controls.Add(this.nudAmplitude);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnSquare);
+            this.Controls.Add(this.btnSin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyGenOscill";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplitude)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrequency)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudAmplitude;
+        private System.Windows.Forms.NumericUpDown nudFrequency;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClearConsole;
+        private System.Windows.Forms.Button btnSawtooth;
+        private System.Windows.Forms.Button btnClearPanel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿//
+// 
+//
+// https://www.codeproject.com/Articles/30180/Simple-Signal-Generator
+//
+//
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGenOscill
+{
+    public partial class Form1 : Form
+    {
+        private string strSignalType = "SINE";
+        private float frequency = 1f;
+        private float amplitude = 75f;
+        float t;  // время
+        float v_prev;   // Значение сигнала в предыдущий момент вермени. Необходимо для прорсовки графика
+        Pen pen = new Pen(Brushes.Lime, 5);  // Ширина линии 3 пикселя
+
+        int x;    // Координата развертки сигнала в пределах компонента panel1
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSin_Click(object sender, EventArgs e) { strSignalType = "SINE"; pen = new Pen(Brushes.Lime, 5); }
+        private void btnTriangle_Click(object sender, EventArgs e) { strSignalType = "TRIANGLE"; pen = new Pen(Brushes.Orange, 5); }
+        private void btnSquare_Click(object sender, EventArgs e)   { strSignalType = "SQUARE"; pen = new Pen(Brushes.Yellow, 5); }
+        private void btnSawtooth_Click(object sender, EventArgs e) { strSignalType = "SAWTOOTH"; pen = new Pen(Brushes.Cyan, 5); }
+
+
+        private void nudFrequency_ValueChanged(object sender, EventArgs e)
+        {
+            frequency = (float)nudFrequency.Value;
+        }
+
+        private void nudAmplitude_ValueChanged(object sender, EventArgs e)
+        {
+            amplitude = (float)nudAmplitude.Value;
+        }
+
+        private float GetValue(float time)
+        {
+            float value = 0f;
+            float t = frequency * time;
+
+            switch (strSignalType)
+            {
+                case "SINE": // sin( 2 * pi * t )
+                    value = (float)Math.Sin(2f * Math.PI * t);
+                    break;
+                case "SQUARE": // sign( sin( 2 * pi * t ) )
+                    value = Math.Sign( Math.Sin(2f * Math.PI * t));   // если у синусоиды знак плюс, то на выходе 1, если минус, то -1
+                    break;
+                case "TRIANGLE":
+                    // 2 * abs( t - 2 * floor( t / 2 ) - 1 ) - 1
+                    value = 1f - 4f * (float)Math.Abs( Math.Round(t - 0.25f) - (t - 0.25f));
+                    break;
+                case "SAWTOOTH":
+                    // 2 * ( t/a - floor( t/a + 1/2 ) )
+                    value = 2f * (t - (float)Math.Floor(t + 0.5f));
+                    break;
+            }
+
+            return (amplitude * value);
+        }
+
+        /*
+        public float GetValue()
+        {
+            float time = (float)(Stopwatch.GetTimestamp() - startTime)
+                            / ticksPerSecond;
+            return GetValue(time);
+        }
+        */
+        //public float GetValue()
+        //{
+        //    //float time = (float)(Stopwatch.GetTimestamp() - startTime)
+        //    //                / ticksPerSecond;
+        //    return GetValue( 0);
+        //}
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //
+            //  RNN: С точным значением частоты сигнала и значением дискретизации
+            //  Результат виден на экране. 
+
+            // Частота сигнала 1 Гц
+            //timer1.Interval = 100;   // Но панели свойств компонента timer1 задано значение 100 мс.
+            //t += 0.1f;   // Дискретизацию (оцифровку сигнала) делаем 10 раз в секунду
+            //t += 0.01f;
+
+            // Частота сигнала 10 Гц
+            timer1.Interval = 10;
+            //t += 0.1f;   // Дискретизацию (оцифровку сигнала) делаем 100 раз в секунду
+            t += 0.01f;
+
+
+            //float A = 50f;
+
+            float v = GetValue( t);     // Передаем функции заданное время и получаем значение, выбранного сигнала для этого момента времени
+
+            //Console.WriteLine( Math.Round(t, 2) + ":   " + v);    // Вывод фазы и значениt сигнала
+            Console.WriteLine( v);     // Вывод только значения сигнала
+
+            Graphics g = panel1.CreateGraphics();
+            
+            //Pen pen = Pens.Lime;    // ширина линии будет один пиксель
+
+            //g.DrawLine(pen, 0, 0, 100, 100);  // DEBUG
+            //g.Clear( panel1.BackColor);   // Перед тем как рисовать новую линию, стираем старую, стирая всю панел paenl1. Очень грубо, но просто
+            //g.DrawLine(pen, 0, 0, x, v* A + panel1.Height/2 );
+
+            //g.DrawLine( pen, x, 0, x, v + panel1.Height / 2);
+
+            int OFF = panel1.Height / 2;
+            g.DrawLine(pen, x-1, v_prev + OFF, x, v + OFF);
+            // panel1.Height/2 -  это центр панели по вертикали
+            // x - 1 - это координата X для предыдущей v_prev точки сигнала
+
+            v_prev = v;  // Сохраняем текущее значение сигнала. На следующем круге оно будет предыдущим
+
+            x += 1;   // Развертка по горизонтали
+            if (x > panel1.Width)
+            {
+                x = 0;
+                g.Clear(panel1.BackColor);  // Очисить панель
+                //panel1.Invalidate();  // Очисить панель можно и так
+
+            }
+            
+        }
+
+        private void btnClearConsole_Click(object sender, EventArgs e)
+        {
+            Console.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();  // DEBUG
+        }
+    }
+}
